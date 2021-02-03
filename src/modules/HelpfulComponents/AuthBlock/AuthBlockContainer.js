410ce984cfc74565c4fd346bf3b0c8ef5d755078
bonnysid <bonnysidworker@gmail.com>
2021-02-03 12:07:03 +0300

    refactor auth block

--- src/modules/HelpfulComponents/AuthBlock/AuthBlockContainer.js
+++ src/modules/HelpfulComponents/AuthBlock/AuthBlockContainer.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import {connect} from "react-redux";
 import AuthBlock from "./AuthBlock";
-import axios from "axios";
-import {setAuthUserInfo, toggleFetching} from "../../../redux/authReducer";
+import {loginRequest} from "../../../redux/authReducer";
 import Preloader from "../Preloader";
 
 class AuthBlockContainer extends React.Component {
 
     componentDidMount() {
-        this.loginRequest();
-    }
-
-    loginRequest = () => {
-
+        this.props.loginRequest();
     }
 
     render() {
         if (this.props.isFetching) return <Preloader/>
 
-        return <AuthBlock {...this.props} loginRequest={this.loginRequest} right={this.props.right}/>
+        return <AuthBlock {...this.props} loginRequest={this.props.loginRequest} right={this.props.right}/>
     }
 }
 
@@ -28,4 +23,4 @@ const mapStateToProps = (state) => ({
     login: state.auth.login
 })
 
-export default connect(mapStateToProps, {setAuthUserInfo, toggleFetching})(AuthBlockContainer);
+export default connect(mapStateToProps, {loginRequest})(AuthBlockContainer);
