e96c1f269049b4ae810d9edfa5276f7b91cbcaa3
bonnysid <bonnysidworker@gmail.com>
2021-02-16 23:05:08 +0400

    refactor profile status class component to func with hooks

--- src/modules/Profile/Description/ProfileStatus.js
+++ src/modules/Profile/Description/ProfileStatus.js
@@ -1,46 +1,42 @@
 import React from 'react';
 import s from './Description.module.css';
+import {useEffect, useState} from "react";
 
-class ProfileStatus extends React.Component {
-    state = {
-        isEditMode: false,
-        status: this.props.status
+const ProfileStatus = (props) => {
+    const [isEditMode, toggleEditMode] = useState(false);
+    const [status, editStatus] = useState('');
+
+    useEffect(() =>  {
+        if (status !== props.status && !isEditMode) editStatus(props.status);
+    }, [status, props.status, isEditMode])
+
+
+    const activateEditMode = () => {
+        toggleEditMode(true);
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.status !== this.props.status && !this.state.isEditMode) this.setState({status: this.props.status});
+    const deactivateEditMode = () => {
+        toggleEditMode(false)
+        props.updateUserStatus(status);
     }
 
-    activateEditMode = () => {
-        this.setState({
-            isEditMode: true
-        })
+    const onInputChange = (e) => {
+        editStatus(e.currentTarget.value);
     }
 
-    deactivateEditMode = () => {
-        this.setState({
-            isEditMode: false
-        })
-        this.props.updateUserStatus(this.state.status);
-    }
 
-    onInputChange = (e) => {
-        this.setState({
-            status: e.currentTarget.value
-        })
-    }
+    return (
+        <div>
+            {isEditMode ?
+                <input className={s.input} onChange={(e) => onInputChange(e)} autoFocus={true}
+                       onBlur={deactivateEditMode} value={status}/>
+                :
+                <p onTouchEnd={activateEditMode}
+                   onDoubleClick={activateEditMode}>{props.status || 'Click to change it'}</p>
+            }
+        </div>
+    )
 
-    render() {
-        return (
-            <div>
-                {this.state.isEditMode ?
-                    <input className={s.input} onChange={(e) => this.onInputChange(e)} autoFocus={true} onBlur={this.deactivateEditMode} value={this.state.status}/>
-                    :
-                    <p onTouchEnd={this.activateEditMode} onDoubleClick={this.activateEditMode}>{this.props.status || 'Click to change it'}</p>
-                }
-            </div>
-        )
-    }
 }
 
 export default ProfileStatus;
