8a500736e5aa383290943db7c891eb5c8cf79108
bonnysid <bonnysidworker@gmail.com>
2021-03-18 01:29:23 +0400

    refactor(profileReducer): from js to ts

--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -1,19 +1,22 @@
 import {profileAPI} from "../API/API";
 import {setHeaderTitle} from "./navbarReducer";
 import {stopSubmit} from "redux-form";
+import {DispatchType, IAuthUserInfo, IPhotos, IProfile} from "../utils/interfaces/interfaces";
 
-const ADD_POST = 'app/profile/ADD_POST';
-const DELETE_POST = 'app/profile/DELETE_POST';
-const SET_USER_PAGE_INFO = 'app/profile/SET_USER_PAGE_INFO';
-const TOGGLE_FETCHING = 'app/profile/TOGGLE_FETCHING';
-const SET_USER_STATUS = 'app/profile/SET_USER_STATUS';
-const SET_PHOTOS = 'app/profile/SET_PHOTOS';
+export const enum ProfileActionTypes {
+    ADD_POST = 'app/profile/ADD_POST',
+    DELETE_POST = 'app/profile/DELETE_POST',
+    SET_USER_PAGE_INFO = 'app/profile/SET_USER_PAGE_INFO',
+    TOGGLE_FETCHING = 'app/profile/TOGGLE_FETCHING',
+    SET_USER_STATUS = 'app/profile/SET_USER_STATUS',
+    SET_PHOTOS = 'app/profile/SET_PHOTOS'
+}
 
 const initialState = {
     posts: [
         {
             id: 1,
-            author: 'Nikita Bortsov',
+            author: 'Nikita Bortsov' as null | string,
             comment: 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.',
             likeCount: 90
         },
@@ -24,14 +27,67 @@ const initialState = {
     userStatus: ''
 };
 
-export const addPost = (authorInfo, message) => ({type: ADD_POST, authorInfo, message});
-export const deletePost = (postId) => ({type: DELETE_POST, postId});
-export const setUserPageInfo = (userPageInfo) => ({type: SET_USER_PAGE_INFO, userPageInfo});
-export const toggleFetching = (isFetching) => ({type: TOGGLE_FETCHING, isFetching});
-export const setUserStatus = (status) => ({type: SET_USER_STATUS, status});
-export const setPhotos = (photos) => ({type: SET_PHOTOS, photos});
+export type ProfileState = typeof initialState
 
-export const getUserInfo = (userId) => async (dispatch) => {
+export type AddPostAction = {
+    type: ProfileActionTypes.ADD_POST,
+    authorInfo: IAuthUserInfo,
+    message: string
+}
+
+export type DeletePostAction = {
+    type: ProfileActionTypes.DELETE_POST,
+    postId: number
+}
+
+export type SetUserPageInfoAction = {
+    type: ProfileActionTypes.SET_USER_PAGE_INFO,
+    userPageInfo: IProfile
+}
+
+export type ToggleFetchingAction = {
+    type: ProfileActionTypes.TOGGLE_FETCHING,
+    isFetching: boolean
+}
+
+export type SetUserStatusAction = {
+    type: ProfileActionTypes.SET_USER_STATUS,
+    status: string
+}
+
+export type SetPhotosAction = {
+    type: ProfileActionTypes.SET_PHOTOS,
+    photos: IPhotos
+}
+
+export type ProfileAction =
+    AddPostAction
+    | DeletePostAction
+    | SetUserStatusAction
+    | SetUserPageInfoAction
+    | ToggleFetchingAction | SetPhotosAction
+
+export const addPost = (authorInfo: IAuthUserInfo, message: string): AddPostAction => ({
+    type: ProfileActionTypes.ADD_POST,
+    authorInfo,
+    message
+});
+export const deletePost = (postId: number): DeletePostAction => ({type: ProfileActionTypes.DELETE_POST, postId});
+export const setUserPageInfo = (userPageInfo: IProfile): SetUserPageInfoAction => ({
+    type: ProfileActionTypes.SET_USER_PAGE_INFO,
+    userPageInfo
+});
+export const toggleFetching = (isFetching: boolean): ToggleFetchingAction => ({
+    type: ProfileActionTypes.TOGGLE_FETCHING,
+    isFetching
+});
+export const setUserStatus = (status: string): SetUserStatusAction => ({
+    type: ProfileActionTypes.SET_USER_STATUS,
+    status
+});
+export const setPhotos = (photos: IPhotos): SetPhotosAction => ({type: ProfileActionTypes.SET_PHOTOS, photos});
+
+export const getUserInfo = (userId?: number) => async (dispatch: DispatchType) => {
     dispatch(toggleFetching(true));
     const data = await profileAPI.getProfileInfo(userId)
     const status = await profileAPI.getUserStatus(userId)
@@ -42,14 +98,14 @@ export const getUserInfo = (userId) => async (dispatch) => {
     dispatch(toggleFetching(false));
 }
 
-export const getUserStatus = (userId) => async (dispatch) => {
+export const getUserStatus = (userId: number) => async (dispatch: DispatchType) => {
     const status = await profileAPI.getUserStatus(userId)
     dispatch(setUserStatus(status))
 }
 
-export const updateUserStatus = (status) => async (dispatch) => {
+export const updateUserStatus = (status: string) => async (dispatch: DispatchType) => {
     dispatch(toggleFetching(true));
-    const data = await profileAPI.updateUserStatus(status)
+    const data: any = await profileAPI.updateUserStatus(status)
 
     if (data.resultCode === 0) {
         dispatch(toggleFetching(false));
@@ -57,17 +113,17 @@ export const updateUserStatus = (status) => async (dispatch) => {
     }
 }
 
-export const savePhoto = (photo) => async (dispatch) => {
-    const data = await profileAPI.savePhoto(photo);
+export const savePhoto = (photo: any) => async (dispatch: DispatchType) => {
+    const data: any = await profileAPI.savePhoto(photo);
 
     dispatch(setPhotos(data.data.photos));
 }
 
-export const saveProfile = (profile) => async (dispatch) => {
+export const saveProfile = (profile: IProfile) => async (dispatch: DispatchType) => {
     // dispatch(toggleFetching(true))
-    const data = await profileAPI.saveProfile(profile);
-    if(data.data.resultCode === 0) {
-        dispatch(getUserInfo());
+    const data: any = await profileAPI.saveProfile(profile);
+    if (data.data.resultCode === 0) {
+        await dispatch(getUserInfo());
     } else {
         stopSubmit('description', {_error: data.messages[0] || 'Some error!'})
     }
@@ -75,15 +131,15 @@ export const saveProfile = (profile) => async (dispatch) => {
 }
 
 
-const profileReducer = (state = initialState, action) => {
+const profileReducer = (state = initialState, action: ProfileAction): ProfileState => {
 
 
     switch (action.type) {
 
-        case ADD_POST:
+        case ProfileActionTypes.ADD_POST:
             const post = {
                 id: state.posts[state.posts.length - 1].id++,
-                author: action.authorInfo.name,
+                author: action.authorInfo.login,
                 comment: action.message,
                 likeCount: 0
             }
@@ -91,10 +147,9 @@ const profileReducer = (state = initialState, action) => {
             return {
                 ...state,
                 posts: [...state.posts, post],
-                newPostText: ''
             };
 
-        case DELETE_POST:
+        case ProfileActionTypes.DELETE_POST:
             state.posts.forEach((item, index) => {
                 if (item.id === action.postId) state.posts.splice(index, 1);
             });
@@ -102,22 +157,22 @@ const profileReducer = (state = initialState, action) => {
                 ...state,
                 posts: [...state.posts]
             };
-        case SET_USER_PAGE_INFO:
+        case ProfileActionTypes.SET_USER_PAGE_INFO:
             return {
                 ...state,
                 userPageInfo: action.userPageInfo
             }
-        case TOGGLE_FETCHING:
+        case ProfileActionTypes.TOGGLE_FETCHING:
             return {
                 ...state,
                 isFetching: action.isFetching
             }
-        case SET_USER_STATUS:
+        case ProfileActionTypes.SET_USER_STATUS:
             return {
                 ...state,
                 userStatus: action.status
             }
-        case SET_PHOTOS:
+        case ProfileActionTypes.SET_PHOTOS:
             return {
                 ...state,
                 userPageInfo: {...state.userPageInfo, photos: action.photos}
--- src/utils/interfaces/interfaces.ts
+++ src/utils/interfaces/interfaces.ts
@@ -1,14 +1,14 @@
 import store from "../../redux/reduxStore";
 
 export interface IAuthUserInfo {
-    id: number,
-    email: string,
-    login: string
+    id: number | null,
+    email: string | null,
+    login: string | null
 }
 
 export interface IMessage {
     message: string,
-    photo: string,
+    photo: string | null,
     userId: number,
     userName: string
 }
@@ -16,6 +16,33 @@ export interface IMessage {
 export interface IMenuItem {
     title: string,
     link: string
+}
+
+export interface IProfile {
+    userId: number | null
+    lookingForAJob: number | null
+    lookingForAJobDescription: number | null
+    fullName: number | null
+    contacts: {
+        github: string | null
+        vk: string | null
+        facebook: string | null
+        instagram: string | null
+        twitter: string | null
+        website: string | null
+        youtube: string | null
+        mainLink: string | null
+    }
+
+}
+
+export interface IPhotos {
+    small: string,
+    large: string
+}
+
+export interface IData {
+    resultCode: number
 }
 
 export type Messages = IMessage[];
