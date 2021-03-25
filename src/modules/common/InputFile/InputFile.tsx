0db60c54ce8dcb596370c694f92e80fc9ba9702e
bonnysid <bonnysidworker@gmail.com>
2021-03-25 21:05:37 +0300

    ref: InputFile -> ts

--- src/modules/common/InputFile/InputFile.tsx
+++ src/modules/common/InputFile/InputFile.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import s from './InputFile.module.css'
 
+interface Props {
+    onClick?: (e: React.MouseEvent<HTMLLabelElement>) => void,
+    onChange: (e: any) => void,
+    placeholder?: string
+}
 
-const InputFile = ({onClick, onChange, placeholder}) => {
+const InputFile: React.FC<Props> = ({onClick, onChange, placeholder}) => {
     return (
         <>
             <input onChange={onChange} id={'file'} type="file" className={s.input}/>
