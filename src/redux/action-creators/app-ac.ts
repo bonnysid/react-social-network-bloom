592e989d7501b87e3f58d787299871b9116cb857
bonnysid <bonnysidworker@gmail.com>
2021-03-25 23:13:40 +0300

    ref: add types for app thunks

--- src/redux/action-creators/app-ac.ts
+++ src/redux/action-creators/app-ac.ts
@@ -1,10 +1,14 @@
 import {AppActionTypes} from "../action-types/app-actions";
 import {loginRequest} from "./auth-ac";
+import {ThunkAction} from "redux-thunk";
+import {State} from "../redux-store";
+import {AppAction} from "../reducers/app-reducer";
 
+export type AppThunk = ThunkAction<Promise<void>, State, undefined, AppAction>
 
 const initializedSuccessful = () => ({type: AppActionTypes.INITIALIZED_SUCCESSFUL});
 
-export const initializeApp = () => async (dispatch: any) => {
+export const initializeApp = (): AppThunk => async (dispatch) => {
     await Promise.all([dispatch(loginRequest())])
     dispatch(initializedSuccessful());
 }
